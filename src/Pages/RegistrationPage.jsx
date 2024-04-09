2dc341f998843eb6f98c339584fbe1c57926901f
RuffledZest <vibhanshalok@gmail.com>
2024-04-09 17:04:23 +0530

    some registration page changes

--- src/Pages/RegistrationPage.jsx
+++ src/Pages/RegistrationPage.jsx
@@ -1,10 +1,12 @@
-import Registration from "../components/Registration";
+// import Registration from "../components/Registration";
+import SubmissionPage from "../components/SubmissionPage";
 
 function RegistrationPage() {
   return (
     <div>
       <h1>Registration Page</h1>
-      <Registration />
+      {/* <Registration /> */}
+      <SubmissionPage />
     </div>
   );
 }
